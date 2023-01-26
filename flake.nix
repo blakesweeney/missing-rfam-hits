d5cc0ec678293db253a315c41240586ee2d65124
Blake Sweeney <bsweeney@ebi.ac.uk>
2023-01-26 12:05:59 +0000

    Basic python env

--- flake.nix
+++ flake.nix
@@ -6,13 +6,17 @@
     personal = {
       url = "git+ssh://git@git.sr.ht/~showyourcode/flakes";
       inputs = {
-        /* nixpkgs.follows = "nixpkgs"; */
+        nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
       };
     };
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, personal, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
@@ -20,18 +24,41 @@
           inherit system;
           overlays = with inputs; [
             devshell.overlay
-            inputs.personal.overlay
+            personal.overlay
+            poetry2nix.overlay
           ];
         };
+        python-env = pkgs.poetry2nix.mkPoetryEnv {
+        projectDir = ./.;
+        python = pkgs.python39;
+        /* overrides = with pkgs; [ */
+        /*   poetry2nix.defaultPoetryOverrides */
+        /*      (self: super: { */
+        /*        click-aliases = super.click-aliases.overrideAttrs(old: { */
+        /*          buildInputs = old.buildInputs ++ [ self.setuptools ]; */
+        /*        }); */
+        /*        obonet = super.obonet.overrideAttrs(old: { */
+        /*          buildInputs = old.buildInputs ++ [ self.setuptools ]; */
+        /*        }); */
+        /*        pypika = super.pypika.overrideAttrs(old: { */
+        /*          buildInputs = old.buildInputs ++ [ self.setuptools ]; */
+        /*        }); */
+        /*      }) */
+        /* ]; */
+  };
       in
       {
         devShell = pkgs.devshell.mkShell {
-          name = "curation-tool-shell";
+          name = "test-missing-rfam-hits-shell";
           motd = "";
 
           packages = with pkgs; [
             infernal
             easel
+            nextflow
+            nodePackages.pyright
+            poetry
+            python-env
           ];
         };
       }
